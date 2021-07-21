57ad7f777d249c1fb87c4a3798c27ea91e796e98
MrAdiaraaa-087 <87253374+MrAdiaraaa-087@users.noreply.github.com>
2021-07-21 20:49:30 +0700

    Add files via upload

---
+++ z.sh
@@ -0,0 +1,142 @@
+#!/bin/sh
+clear
+# code warna
+biru="\033[1;96m"
+kuning="\033[1;93m"
+merah="\033[1;91m"
+hijau="\033[1;92m"
+abu2="\033[1;90m"
+ungu="\033[1;95m"
+sleep 1
+clear
+figlet WELCOME | lolcat
+echo "${hijau}SELAMAT DATANG TO TOOLS MRAdiaraaa"
+sleep 3
+clear
+sleep 2
+figlet HACKDARK | lolcat
+echo "${biru}______________________________"
+echo
+echo "${hijau}Author ${biru}= ${kuning}MRAdiaraaa-087"
+echo "${hijau}Team   ${biru}= ${kuning}CyberAttacker"
+echo "${hijau}Github ${biru}= ${kuning}Adii030107"
+echo "${biru}______________________________ "
+echo
+echo "${abu2}[${biru}1${abu2}]${biru}----------------------"
+echo "   ${kuning}SADAP WA DGN NMR"
+echo "                         ${biru}---------------------${abu2}[${biru}2${abu2}]"
+echo "${abu2}[${biru}3${abu2}]${biru}---------------------- ${kuning}HACK PULSA ALL OPP-"
+echo "   Mempercepat Koneksi"
+echo "                         ${biru}---------------------${abu2}[${biru}4${abu2}]"
+echo "${abu2}[${biru}5${abu2}]${biru}---------------------- ${kuning}Virus Perusak Via IP"
+echo "   ${kuning}KELUAR"
+echo "${biru}_____________________________________________________"
+echo "${hijau} "
+read -p "~⟩ " nazila
+
+if [ $nazila = 1 ]
+then
+clear
+sleep 1
+echo "${merah}GUNAKAN DENGAN ${hijau}BIJAK"
+sleep 3
+clear
+sleep 3
+echo "SCRIPT INI WORK HANYA 1 ANDROID 1 KALI"
+sleep 3
+echo "MAKA GUNAKAN TOOLS DENGAN BIJAK!!!!!!"
+sleep 10
+clear
+sleep 1
+figlet DARK-WA | lolcat
+echo "${merah}-----------------------------"
+echo "${merah}DARK WHATSAPP TO NUMBER"
+echo "${abu2}NO TARGET DIAWALI 62××:"
+read name2
+echo "${kuning}LOADING...."
+sleep 5
+echo "${ungu}SEDANG MENGIRIM SMS....."
+sleep 10
+echo "${hijau}WHATSAPP - [1381] adalah kode verifikasi akun anda"
+echo "demi keamanan mohon untuk tidak memberitahukan kode ini kepada siapapun"
+fi
+
+if [ $nazila = 2 ]
+then
+clear
+figlet Pulsa | lolcat
+sleep 1
+echo "${merah}-----------------------"
+echo "${biru}FREE PULSA ALL OPP"
+echo "${merah}-----------------------"
+echo
+echo
+echo
+echo
+echo
+echo "${hijau}======================="
+echo "${biru}HACK PULSA"
+echo "${biru}NOMOR TARGET :"
+read name5
+echo "${hijau}MINIMAL 50K KEBAWAH"
+echo "USAHAKAN JGN TERLALU BAR-BAR"
+echo "AGAR SCRIPT TIDAK MUDAH RUSAK"
+sleep 4
+echo "JUMLAH PULSA = "
+read name77
+echo "${biru}Loading....."
+sleep 10
+echo "${merah}TUNGGU BEBERAPA JAM SAMPAI PULSA MASUK"
+sleep 1
+echo "JIKA TIDAK MASUK BERARTI SCRIPT SUDAH TIDAK WORK"
+echo "Atau Anda Belum BERUNTUNG!!"
+fi
+
+if [ $nazila = 3 ]
+then
+clear
+sleep 2
+echo "${hijau}SEDANG MEMPERBAIKI JARINGAN....."
+sleep 15
+echo "SELESAI...."
+sleep 5
+clear
+sleep 1
+echo "SEKARANG ONN KAN MODE PESAWAT"
+sleep 1
+echo "JIKA SUDAH TINGGAL TUNGGU 5 DTK"
+sleep 1
+echo "LALU ANDA OFF KAN LAGI"
+sleep 1
+echo "LANGSUNG ONN DATA, ENJOY SPEED TO INTERNET"
+fi
+
+if [ $nazila = 4 ]
+then
+clear
+sleep 4
+echo "${merah}USAHAKAN ANDA SUDAH MENGETAHUI"
+echo "IP TARGET KORBAN, HATI2 DGN SCRIPT INI"
+echo "CREATOR TIDAK BERTANGGUNG JAWAB!!!!"
+echo "------------------------------------------"
+echo "IP ADDRESS TARGET -"
+read name24
+sleep 3
+echo "${biru}Saya Saran kan Untuk Tidak Keluar Apk"
+echo "Ini Jika Ingin Berjalan Lancar"
+sleep 3
+echo "SEND TROJANS TO IP"
+sleep 3
+echo "Loading...."
+sleep 9999999999999999999999
+echo "SELAMAT ANDA KENA PRANK"
+fi
+
+if [ $nazila = 5 ]
+then
+clear
+sleep 2
+echo "IKAN LELE, IKAN HIYU"
+echo "FUCK YOU ><"
+fi
+
